37bcb76a16cbe97bd1e1ac8a699ffb43e7f89061
pashinskij99 <pashinskij99@icloud.com>
2023-04-27 22:11:49 +0300

    return next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-// const withSass = require('@zeit/next-sass')
-// const withCSS = require("@zeit/next-css");
-module.exports = {
+const withSass = require('@zeit/next-sass')
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS(withSass({
     webpack(config, options) {
         config.module.rules.push({
             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
@@ -14,4 +14,4 @@ module.exports = {
 
         return config;
     }
-};
+}));
