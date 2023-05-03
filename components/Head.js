9c0658390941e8f3d35be2b96001498eee34cae2
pashinskij99 <pashinskij99@icloud.com>
2023-05-03 11:53:24 +0300

    add changes

--- components/Head.js
+++ components/Head.js
@@ -4,11 +4,16 @@ const SeoHead = ({title, description}) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="shortcut icon" href="/assets/icons/favicon-onliq.svg" />
-      <meta property="og:title" content={title} key="title" />
-      <meta property="og:description" content={description} key="description" />
+      <link rel="icon" href="/assets/icons/favicon-onliq.svg" />
+      <meta property="title" content={title} />
+      <meta property="description" content={description} />
     </Head>
   )
+}
+
+SeoHead.defaultProps = {
+  title: 'Onliq.Agency',
+  desc: "Onliq.Agency recognizes the significance of dependable management for the progress and expansion of OnlyFans. As the top agency to offer such services, we remain informed and knowledgeable about the most recent developments in this constantly evolving digital environment. Our adept blend of dependable marketing solutions and comprehensive comprehension of the OnlyFans algorithm guarantees that our clients always have an advantage over their rivals.",
 }
 
 export default SeoHead
